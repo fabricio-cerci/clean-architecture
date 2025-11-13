8a7fe8d93ffa63e650f1e8c297cfa1329a6134ed
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2025-11-12 21:58:53 -0400

    feat: add usecase for updating customer

---
+++ src/usecase/customer/update/update.customer.usecase.ts
@@ -0,0 +1,31 @@
+import Address from "../../../domain/customer/value-object/address";
+import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
+import { InputUpdateCustomerDto, OutputUpdateCustomerDto } from "./update.customer.dto";
+
+export default class UpdateCustomerUseCase {
+  private customerRepository: CustomerRepository;
+
+  constructor(customerRepository: CustomerRepository) {
+    this.customerRepository = customerRepository;
+  }
+
+  async execute(input: InputUpdateCustomerDto): Promise<OutputUpdateCustomerDto>{
+    const customer = await this.customerRepository.find(input.id);
+
+    customer.changeName(input.name);
+    customer.changeAddress(new Address(input.address.street, input.address.number, input.address.zip, input.address.city));
+
+    await this.customerRepository.update(customer);
+
+    return {
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: customer.address.street,
+        number: customer.address.number,
+        zip: customer.address.zip,
+        city: customer.address.city
+      }
+    };
+  }
+}
