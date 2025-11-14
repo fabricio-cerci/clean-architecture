f2217091fdf31a1e9d23b04cd8e5fd162694fb35
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2025-11-13 22:11:28 -0400

    feat: implements notification pattern on product and add tests

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -2,15 +2,27 @@ import Product from "./product";
 
 describe("Product unit tests", () => {
     it("should throw error when id is empty", () => {
-        expect(() => new Product("", "Product 1", 100)).toThrow("Id is required");
+        expect(() => new Product("", "Product 1", 100)).toThrow("product: Id is required");
     });
 
     it("should throw error when name is empty", () => {
-        expect(() => new Product("123", "", 100)).toThrow("Name is required");
+        expect(() => new Product("123", "", 100)).toThrow("product: Name is required");
     });
 
     it("should throw error when price is less than zero", () => {
-        expect(() => new Product("123", "123", -1)).toThrow("Price must be greater than zero");
+        expect(() => new Product("123", "123", -1)).toThrow("product: Price must be greater than zero");
+    });
+
+    it('should throw error when id and name are empty', () => {
+        expect(() => new Product("", "", 100)).toThrow("product: Id is required,product: Name is required");
+    });
+
+    it('should throw error when name is empty and price is less than zero', () => {
+        expect(() => new Product("123", "", -1)).toThrow("product: Name is required,product: Price must be greater than zero");
+    });
+
+     it('should throw error when id and name are empty and price is less than zero', () => {
+        expect(() => new Product("", "", -1)).toThrow("product: Id is required,product: Name is required,product: Price must be greater than zero");
     });
 
     it("should change name", () => {
