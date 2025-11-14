7d1704bc5cf4d544490db97498244a43ca844b46
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2025-11-13 21:15:07 -0400

    feat: create product route with creation route and tests

---
+++ src/infrastructure/api/routes/product.route.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import CreateProductUseCase from "../../../usecase/product/create/create.product.usecase";
+import ProductRepository from "../../product/repository/sequelize/product.repository";
+
+export const productRoute = express.Router();
+
+productRoute.post("/", async (req: Request, res: Response) => {
+  const useCase = new CreateProductUseCase(new ProductRepository());
+
+  try {
+    const input = {
+      name: req.body.name,
+      price: req.body.price
+    };
+
+    const output = await useCase.execute(input);
+    res.send(output);
+  } catch (error) {
+    console.log(error);
+  }
+});
