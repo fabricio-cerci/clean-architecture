3663e94992b44cef81b703a764f866832bb02089
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2025-11-15 16:32:17 -0400

    feat: implements yup validator for product on a decoupled way using interface

--- src/domain/customer/factory/customer.validator.factory.ts
+++ src/domain/customer/factory/customer.validator.factory.ts
@@ -1,9 +1,9 @@
-import ValidatiorInterface from "../../@shared/validator/validator.interface";
+import ValidatorInterface from "../../@shared/validator/validator.interface";
 import Customer from "../entity/customer";
 import CustomerYupValidator from "../validator/customer.yup.validator";
 
 export default class CustomerValidatorFactory{
-    static create(): ValidatiorInterface<Customer> {
+    static create(): ValidatorInterface<Customer> {
         return new CustomerYupValidator();
     }
 }
--- src/domain/customer/validator/customer.yup.validator.ts
+++ src/domain/customer/validator/customer.yup.validator.ts
@@ -1,8 +1,8 @@
-import ValidatiorInterface from "../../@shared/validator/validator.interface";
+import ValidatorInterface from "../../@shared/validator/validator.interface";
 import Customer from "../entity/customer";
 import * as yup from "yup";
 
-export default class CustomerYupValidator implements ValidatiorInterface<Customer>{
+export default class CustomerYupValidator implements ValidatorInterface<Customer>{
   validate(entity: Customer): void {
     try {
       yup
